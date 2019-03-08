b13c455f5b7ee302fc6ef906c1ab7af09240a8d8
Paulo Ortolan <paulo.ortolan@gmail.com>
2019-03-08 16:32:59 +0000

    Fixing code

--- src/main/java/org/portolan/ppkp/robot/fight/service/UserInfoService.java
+++ src/main/java/org/portolan/ppkp/robot/fight/service/UserInfoService.java
@@ -4,7 +4,7 @@ import org.portolan.ppkp.robot.fight.entities.UserInfo;
 import org.portolan.ppkp.robot.fight.persistence.UserInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.tools.java.Environment;
+import org.springframework.core.env.Environment;
 
 import java.util.Date;
 
@@ -22,18 +22,10 @@ public class UserInfoService {
     }
 
     public boolean validateUserAccess(String ip, String path) {
-      // get the IP = OK
-      // search for the IP
-      // If found
-      //    verify if accesses for that IP is > daily.accessCount for this day
-      //        TRUE » Return false
-      //        FALSE » Add 1 to accessCount save the record
-      // Not found
-      //    add a new userinfo
         int accesses = repository.todayAccesses(ip);
-        int maxAccesses = environment.getgetProperty("");
+        int maxAccesses = Integer.parseInt(environment.getProperty("daily.accessCount"));
 
-        if(accesses ==) {
+        if(accesses <= maxAccesses) {
             UserInfo userInfo = new UserInfo();
 
             userInfo.setIp(ip);
@@ -43,9 +35,7 @@ public class UserInfoService {
 
             return true;
         } else {
-
+            return false;
         }
-
-        return false;
     }
 }
